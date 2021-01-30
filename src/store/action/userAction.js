3a5243602c7868e3b6f4b3e84d307322db834b03
albarmo <moerhamsa@gmail.com>
2021-01-30 18:02:49 +0700

    fixed login - register-profile-image error

--- src/store/action/userAction.js
+++ src/store/action/userAction.js
@@ -1,8 +1,8 @@
 import axios from "axios";
+import swal from "sweetalert";
 
 export const userLogin = (email, password) => {
   return (dispatch) => {
-    console.log(email, password, "dari user action");
     axios({
       method: "POST",
       url: "http://localhost:5000/user/login",
@@ -12,11 +12,61 @@ export const userLogin = (email, password) => {
       },
     })
       .then(({ data }) => {
+        console.log("success login", data);
         dispatch({ type: "LOGIN", payload: data });
         localStorage.setItem("access_token", data.access_token);
       })
       .catch((error) => {
+        console.log("failed login");
         console.log(error);
+        dispatch({ type: "LOGOUT" });
+        swal("Ops!", "email or password wrong!");
       });
+  };
+};
+
+export const userRegister = (payload) => {
+  console.log(payload, "action");
+  return (dispatch) => {
+    const {
+      classType,
+      email,
+      fileList,
+      fullname,
+      gender,
+      password,
+      phone_number,
+      school,
+    } = payload;
+
+    console.log(email);
+    axios({
+      method: "POST",
+      url: "http://localhost:5000/user/register",
+      data: {
+        fullname: fullname,
+        profile_image: fileList,
+        phone_number: phone_number,
+        email: email,
+        gender: gender,
+        class: classType,
+        school: school,
+        password: password,
+      },
+    })
+      .then(({ data }) => {
+        console.log("success register new user", data);
+        swal("Success Created new Account!", data.email);
+      })
+      .catch((error) => {
+        console.log(error, "error while register - userAction");
+      });
+  };
+};
+
+export const userLogout = () => {
+  return (dispatch) => {
+    console.log("userlogout action");
+    dispatch({ type: "LOGOUT" });
   };
 };
